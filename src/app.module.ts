dfa3446592c27c1ce8bc8c6fb728303b92100485
Вадим Видман <vadimvidman@MacBook-Air-Vadim.local>
2023-02-20 12:40:55 +0400

    init serve-static and multer, change gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
 import { CalculatorModule } from './calculator/calculator.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [NewsModule, CalculatorModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    NewsModule,
+    CalculatorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
